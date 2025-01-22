94a8b461c48009f66c343d536f56d5e8d2c7ff85
Gülay ADIGÜZEL <glyadgzl@hotmail.com>
2025-01-22 22:47:04 +0300

    Implement note creation and setup for notes list in MainActivity

--- app/src/main/java/com/example/notelifecycle/MainActivity.kt
+++ app/src/main/java/com/example/notelifecycle/MainActivity.kt
@@ -13,13 +13,31 @@ class MainActivity : AppCompatActivity() {
        
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnNewNote.setOnClickListener {
+            val intent = Intent(this, NewNoteActivity::class.java)
+            startActivity(intent)
+
+            setupNotesList()
+        }
+
+        private fun startNoteActivity() {
+            val intent = Intent(this, NoteActivity::class.java)
+           startActivityForResult(intent, NEW_REQUEST_CODE)
+        }
+
+        private fun setupNotesList() {
+          
+            val adapter= NotesAdapter(mutableListOf()){note ->
+            val intent = Intent(this, NoteActivity::class.java)
+            intent.putExtra(NOTE_EXTRA, note)
+            startActivityForResult(intent, EDIT_REQUEST_CODE)}
+        }
+        binding.recyclerViewNotes.adapter = adapter
+        binding.recyclerViewNotes.layoutManager = LinearLayoutManager(this)
         
 
-
-
-
-
-
-
     }
+
+ 
 }
