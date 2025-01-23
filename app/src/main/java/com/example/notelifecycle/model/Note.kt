2a6acd0e0c87d20ea2ff1fdd86020b6cf1931938
Gülay ADIGÜZEL <glyadgzl@hotmail.com>
2025-01-23 13:13:50 +0300

    Add Note data class with Parcelable implementation

---
+++ app/src/main/java/com/example/notelifecycle/model/Note.kt
@@ -0,0 +1,31 @@
+data class Note(
+    val id:Long=0,
+    val title: String,
+    val content: String,
+    val lastEditTime: String
+):Parcelable{
+    constructor(parcel: Parcel) : this(
+        parcel.readLong(),
+        parcel.readString().toString(),
+        parcel.readString().toString(),
+        parcel.readString().toString()
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeLong(id)
+        parcel.writeString(title)
+        parcel.writeString(content)
+        parcel.writeString(lastEditTime)
+    }
+    override fun describeContents(): Int {
+        return 0
+    }
+    companion object CREATOR : Parcelable.Creator<Note> {
+        override fun createFromParcel(parcel: Parcel): Note {
+            return Note(parcel)
+        }
+        override fun newArray(size: Int): Array<Note?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
